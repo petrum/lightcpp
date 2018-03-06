00d48aa35b08470331ba01d0095c67404a9d4cf7
Petru Marginean <petru.marginean@gmail.com>
2018-03-06 07:21:46 -0500

    misc

--- lightcpp.h
+++ lightcpp.h
@@ -3,6 +3,48 @@
 #include <stdexcept>
 #include <sstream>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iomanip>
+
+std::string GetEnv(const char* pName, const char* pDefault);
+enum TLogLevel {logERROR, logWARNING, logINFO, logDEBUG, logALL};
+TLogLevel FromString(const std::string& level);
+
+class FileLog
+{
+ public:
+    FileLog(TLogLevel level) : messageLevel(level)
+    {
+        os << std::setprecision(16);
+    }
+    ~FileLog();
+    std::ostringstream& operator()(TLogLevel level = logINFO);
+    static FILE*& getFile()
+    {
+        static FILE* pStream = stderr;
+        return pStream;
+    }
+    static TLogLevel& getReportingLevel()
+    {
+        static TLogLevel reportingLevel = FromString(GetEnv("LOG_LEVEL", "INFO"));
+        return reportingLevel;
+    }
+    static void write(const std::string& msg)
+    {   
+        fprintf(getFile(), "%s", msg.c_str());
+        fflush(getFile());
+    }
+private:
+    FileLog(const FileLog&);
+    FileLog& operator =(const FileLog&);
+private:
+    static TLogLevel reportingLevel;
+    TLogLevel messageLevel;
+    std::ostringstream os;
+};
+
+#define FILE_LOG(messageLevel) if (messageLevel > FileLog::getReportingLevel()) ; else FileLog(messageLevel)
 
 template <typename T>
 T enforce(T t, const char* e, const char* f, int n)
@@ -20,10 +62,33 @@ T enforce(T t, const char* e, const char* f, int n)
 #define STD_FUNC_BEGIN try {
 #define STD_FUNC_END } catch (const std::exception& e) \
 {\
-    std::cerr << e.what() << std::endl;\
+    FILE_LOG(logERROR) << e.what();\
 }\
 catch (...)\
 {\
-    std::cerr << "Non standard error" << std::endl;\
+    FILE_LOG(logERROR) << "Non standard error";\
+}
+
+inline std::string GetEnv(const char* pName, const char* pDefault)
+{   
+    ENFORCE(pName);
+    char* pRes = getenv(pName);
+    ENFORCE(pRes || pDefault);
+    return pRes ? pRes : pDefault;
+}
+
+inline TLogLevel FromString(const std::string& level)
+{   
+    if (level == "ERROR")
+        return logERROR;
+    if (level == "WARNING")
+        return logWARNING;
+    if (level == "INFO")
+        return logINFO;
+    if (level == "DEBUG")
+        return logDEBUG;
+    if (level == "ALL")
+        return logALL;
+    return logINFO;
 }
 
